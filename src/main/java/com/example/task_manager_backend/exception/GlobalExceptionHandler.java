00a056f26fc6e3305deec1e302b34f678643b117
varsha <Jan1user240@rpsconsulting.in>
2026-02-06 22:15:36 +0530

    Added global exception handler

---
+++ src/main/java/com/example/task_manager_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.task_manager_backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>(
+                "Something went wrong: " + ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+}
